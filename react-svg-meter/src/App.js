1451374585e40abf127ca44ea919a118cd516168
godofhavoc <akshay163@gmail.com>
2017-08-30 17:01:38 +0530

    first commit

---
+++ react-svg-meter/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Meter from './Meter';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Meter />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ react-svg-meter/src/Meter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+var Meter = function (props) {
+    var {
+        percent = 0,
+        width = 100,
+        height = 10,
+        rounded = true,
+        color = "#0078bc",
+        animate = false,
+        label = null
+    } = props;
+
+    return(
+        <div>test</div>
+    )
+}
+
+export default Meter;
